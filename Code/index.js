392c1f081aa23f037aaa4cf73e3b8e3b63e34f7f
jojo2357 <66704796+jojo2357@users.noreply.github.com>
2020-12-02 18:43:38 -0800

    First!

---
+++ Code/index.js
@@ -0,0 +1,83 @@
+const https = require('https');
+const fs = require('fs');
+const { JSDOM } = require("jsdom");
+const { window } = new JSDOM("");
+const $ = require('jquery')(window);
+
+let watchedTime = 0;
+var videosParsed = 0;
+var vidData = require('../watch-history.json');
+
+vidData = vidData.filter(vid => Date.now() - Date.parse(vid.time) < 31536000000 && vid.title != 'Watched a video that has been removed');
+
+doingRecursion(0);
+
+function doingRecursion(index){
+    if (index == vidData.length)
+        return;
+    var vid = vidData[index];
+    var req = https.get('https://www.googleapis.com/youtube/v3/videos?part=contentDetails&key=AIzaSyDgz4B66rT6tOClZxjzzO7UW69Stg4jUBc&id=' + vid.titleUrl.split('watch?v\u003d')[1], (res) => {
+        var json = '';
+
+        res.on('data', function (chunk) {
+            json += chunk;
+        });
+
+        res.on('end', function () {
+            if (res.statusCode === 200) {
+                try {
+                    var data = JSON.parse(json);
+                    // data is available here:
+                    videosParsed++;
+                    //console.log(data);
+                    watchedTime += strangeStringToTime(data.items[0].contentDetails.duration);
+                    process.stdout.write('\r');
+                    process.stdout.write(formatSecondsAsTime(watchedTime) + ` (${watchedTime})` + " => " + (100 * videosParsed / vidData.length).toPrecision(4) + "%");
+                } catch (e) {
+                    console.log('Error parsing JSON!');
+                }
+            } else {
+                console.log(res);
+                console.log('Status:', res.statusCode);
+            }
+            doingRecursion(index + 1);
+        });
+    });
+}
+
+function strangeStringToTime(strangeString = "") {
+    //var holdover = strangeString;
+    var time = 0;
+    strangeString = strangeString.replace('PT', '');
+    if (strangeString.includes("H")) {
+        time += 3600 * parseInt(strangeString.charAt(0) + (strangeString.charAt(1) == 'H' ? '' : strangeString.charAt(1)))
+        strangeString = strangeString.substring(strangeString.indexOf('H') + 1, strangeString.length);
+    }
+    if (strangeString.includes("M")) {
+        time += 60 * parseInt(strangeString.charAt(0) + (strangeString.charAt(1) == 'M' ? '' : strangeString.charAt(1)))
+        strangeString = strangeString.substring(strangeString.indexOf('M') + 1, strangeString.length);
+    }
+    if (strangeString.includes("S")) {
+        time += parseInt(strangeString.charAt(0) + (strangeString.charAt(1) == 'S' ? '' : strangeString.charAt(1)))
+        strangeString = strangeString.substring(strangeString.indexOf('S') + 1, strangeString.length);
+    }
+    //console.log(holdover + ': ' + time);
+    return time;
+}
+
+function formatSecondsAsTime(secs) {
+    var hr = Math.floor(secs / 3600),
+        min = Math.floor((secs - (hr * 3600)) / 60),
+        sec = Math.floor(secs - (hr * 3600) - (min * 60));
+
+    if (hr < 10) {
+        hr = "0" + hr;
+    }
+    if (min < 10) {
+        min = "0" + min;
+    }
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
+    return hr + ':' + min + ':' + sec;
+};
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Youtube view time getter
- provide this lil guy with your youtube watch data and he will calculate the amount of time youve watched
+# Overview:
+Spotify basically did their version of a "year in review" for all its users. This is my attempt to do that for youtube listeners.
+
+I have two accounts, one for music, the other for leisure. There is no feature to look at only music. More unfortunately is that Youtube doesn't keep how long you watch a video for after two weeks ([sounds like another problem](https://www.github.com/jojo2357/GitHub-Views-Logger)) so this just sums the length of all the videos that you have watched in the past year.
+
+# Setup:
+### Creating an API token:
+Create a project in the google developer console.
+
+Go to the google developer portal and activate the youtube data api v3
+
+Go to the dashboard and go to credentials, then create an api token.
+
+Place the token in clientSecrets.json (please format it right)
+
+### Getting your youtube data:
+In order to prevent the struggle of oauth for each user, we instead manually download the data.
+
+Head over to [google takeout](https://takeout.google.com/) and select **only** YouTube and YouTube Music
+
+Click `Multiple formats` and on the only dropdown not greyed out select `json`
+
+Where it says `All YouTube data included`, click that then deselect everything except `history`
+
+Export the data and when you get the zip, unzip it and copy-paste `watch-history.json` into the root directory of this project
+
+Run index.js and it should work!
